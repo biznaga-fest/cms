265073bc98365f6ed655cc989864bf4bb57e3b33
David Rojo <davidrojo1997@gmail.com>
2025-11-14 15:56:41 +0100

    feat: add publisher plugin

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,12 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+import type { Core } from "@strapi/types";
+
+export default ({ env }): Core.Config.Server => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
+  },
+  cron: {
+    enabled: true,
   },
 });
