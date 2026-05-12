c3399df6a47a750ea49c4e53d7ad10c23a86b7f1
David Rojo <davidrojo1997@gmail.com>
2026-05-12 21:41:47 +0200

    feat: add nested population

--- src/api/info/controllers/info.ts
+++ src/api/info/controllers/info.ts
@@ -2,6 +2,37 @@
  * info controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::info.info');
+const buildDeepPopulate = (uid: string) => {
+  const { attributes } = strapi.contentType(uid as any);
+  const populate: Record<string, any> = {};
+
+  for (const [name, attr] of Object.entries(attributes) as [string, any][]) {
+    if (
+      attr.type === "relation" ||
+      attr.type === "media" ||
+      attr.type === "dynamiczone" ||
+      attr.type === "component"
+    ) {
+      populate[name] = { populate: "*" };
+    }
+  }
+
+  return populate;
+};
+
+export default factories.createCoreController(
+  "api::info.info",
+  ({ strapi }) => ({
+    async find(ctx) {
+      ctx.query = {
+        ...ctx.query,
+        populate:
+          ctx.query.populate ?? (buildDeepPopulate("api::info.info") as any),
+      };
+
+      return await super.find(ctx);
+    },
+  }),
+);
